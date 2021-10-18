7f87a3d9fdfb53d34d3cfdea225370e12a508f95
Ikchhit Kumar Pandey <89184573+wizeewig@users.noreply.github.com>
2021-10-18 18:51:52 +0530

    Add files via upload

---
+++ java/com/example/android/courtcounterbasketball/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.android.courtcounterbasketball;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.courtcounter_basketball.R;
+
+public class MainActivity extends AppCompatActivity {
+    int scorea=0;
+    int scoreb=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+    public void score3a(View view) {
+        scorea=scorea+3;
+        displayteama(scorea);}
+    public void score3b(View view) {
+        scoreb=scoreb+3;
+        displayteamb(scoreb);}
+    public void score2a(View view) {
+        scorea=scorea+2;
+        displayteama(scorea);}
+    public void score2b(View view) {
+        scoreb=scoreb+2;
+        displayteamb(scoreb);}
+    public void freea(View view) {
+        scorea=scorea+1;
+        displayteama(scorea);}
+    public void freeb(View view) {
+        scoreb=scoreb+1;
+        displayteamb(scoreb);}
+    public void reset(View view) {
+        scorea=0;
+        scoreb=0;
+        displayteama(scorea);
+        displayteamb(scoreb);}
+    public void  displayteama(int score)
+    {
+        TextView scoreView= (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+    public void  displayteamb(int score)
+    {
+        TextView scoreView= (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+
+}
